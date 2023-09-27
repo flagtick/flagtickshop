92aacb648741784e505d36c8e6f16f68251ccd45
vuongluis <vuongluis@gmail.com>
2023-09-27 20:50:15 +0700

    [] (Lorence) Optimize code

---
+++ wp-content/plugins/meta-box/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wpmetabox/meta-box',
+        'pretty_version' => '5.7.2',
+        'version' => '5.7.2.0',
+        'reference' => 'c8e0599c041dd43b12d03aad2cfb92faac973877',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wpmetabox/meta-box' => array(
+            'pretty_version' => '5.7.2',
+            'version' => '5.7.2.0',
+            'reference' => 'c8e0599c041dd43b12d03aad2cfb92faac973877',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
